b9c6565171b2f15de852f2bb4f1678a5f2b1cf38
SyedAnees09 <137610555+SyedAnees09@users.noreply.github.com>
2023-10-03 19:22:00 +0530

    Update app.c

--- app.c
+++ app.c
@@ -5,10 +5,15 @@ int main()
 	int cs;
 	while(1)
 	{
-	printf("Program to be executed\n");
+	printf("Select your choice\n");
+	printf("1.program for addition\n");
+	printf("2.program for subtraction\n");
+	printf("3.program for product\n");
+	printf("4.program for division\n");
+	printf("5.program for modulus\n");
+	printf("6.To exit \n")
 	printf("Select the program option to be executed from 1-6\n");
 	scanf("%d",&cs);
-
 	switch(cs)
 		{
 			case 1:
